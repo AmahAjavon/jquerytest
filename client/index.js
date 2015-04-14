f02fbf3061ef68302170f455c4d0fc8644963ec5
AmahAjavon <aj_ajavon@hotmail.com>
2015-04-14 14:42:37 -0700

    simple test

---
+++ client/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+$(document).ready(init);
+
+function init(){
+  $('#getRandom').click(getRandom);
+  $('#evens').on('click', '.box', root);
+}
+
+function getRandom(){
+  $.getJSON('https://qrng.anu.edu.au/API/jsonI.php?length=20&type=uint8', function(response){
+    console.log(response.data);
+
+    var arr = response.data;
+
+    arr.forEach(function(e) {
+      var $div = $('<div>');
+      $div.addClass('box');
+      if(e % 2 === 0) {
+        $div.text(e);
+        $('#evens').append($div);
+      } else if(e % 3 === 0) {
+
+        $div.text(e);
+        $('#threes').append($div);
+      }
+      else {
+        $div.text(e);
+        $('#others').append($div);
+
+      }
+    })
+  });
+}
+
+    function root(){
+      var text = $(this).text();
+      $(this).text(Math.sqrt(text).toPrecision(3));
+    }
